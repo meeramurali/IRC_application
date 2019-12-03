7dbac15ab907ad6bd68c49b51d717bd3c87f0ce2
Meera Murali <mmurali@pdx.edu>
2019-12-02 21:46:03 -0800

    Created User class

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, username, IP_addr, connection):
+        self.username = username
+        self.IP_addr = IP_addr
+        self.connection = connection
+
+    def display_user(self, verbose=False):
+        if verbose:
+            print(f"[{self.username}] <{self.IP_addr}>")
+        else:
+            print(f"[{self.username}]")
+            
+
+# test = User("mmurali", "0.0.0.0", None)
+# test.display_user(verbose=True)

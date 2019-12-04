e1e7844f7a73955ea2a47531a7f9d2d7c254c9d7
Meera Murali <mmurali@pdx.edu>
2019-12-03 22:48:23 -0800

    Updated broadcast function to stringify json packet

--- chatroom.py
+++ chatroom.py
@@ -15,36 +15,16 @@ class Chatroom:
             del self.users[username]
 
 
-    def display_room(self, verbose=False):
-        print(f"#{self.name}")
-        for _, user in self.users.items():
-            user.display_user(verbose)
+    # def display_room(self, verbose=False):
+    #     print(f"#{self.name}")
+    #     for _, user in self.users.items():
+    #         user.display_user(verbose)
 
 
-    def get_users_list(self):
-        users_list = "-----------------------------\n" \
-                    + f"#{self.name} users\n" \
-                    + ".............................\n"
-        for _, user in self.users.items():
-            users_list = users_list + user.username + '\n'
-        users_list = users_list + "-----------------------------"
-        return users_list
-
-
-    def broadcast(self, message): 
+    def broadcast(self, packet): 
         for _, user in self.users.items(): 
             try: 
-                user.connection.sendall(message.encode('utf-8')) 
+                user.connection.sendall(packet.get_json_str().encode('utf-8')) 
             except: 
                 user.connection.close() 
                 self.remove_user(user.username) 
-
-
-# test = Chatroom("CS590")
-# test.add_user(User("mmurali", "0.0.0.0", None))
-# test.add_user(User("diganta", "1.0.0.0", None))
-# test.add_user(User("mahesh", "2.0.0.0", None))
-# test.display_room()
-# print("------------------------")
-# test.remove_user("mahesh")
-# test.display_room(verbose=True)

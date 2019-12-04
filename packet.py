2ba2c248989b96b4e2138d0379a6727cc9cb35b7
Meera Murali <mmurali@pdx.edu>
2019-12-03 22:50:20 -0800

    Defined server packet classes and opcodes

--- packet.py
+++ packet.py
@@ -1,12 +1,5 @@
 import json
 
-SERVER_OPCODE = {
-	'ERROR' : 0,
-	'WELCOME' : 1,
-	'LIST_ROOMS_RESP' : 3,
-	'LIST_USER_RESP' : 5,
-	'TELL_MSG' : 9
-}
 
 CLIENT_OPCODE = {
 	'JOIN_ROOM': 10,
@@ -18,8 +11,21 @@ CLIENT_OPCODE = {
 	'EXIT': 13
 }
 
+SERVER_OPCODE = {
+	'WELCOME' : 1,
+	'CREATE_ROOM_RES': 14,
+	'JOIN_ROOM_RES': 15,
+	'LEAVE_ROOM_RES': 16,
+	'LIST_ROOMS_RESP' : 3,
+	'LIST_USERS_RESP' : 5,
+	'TELL_MSG' : 9,
+	'ERROR' : 0
+}
+
+
+# Base Packet class
 class Packet:
-	def __init__(self, opcode, data, username, roomname):
+	def __init__(self, opcode, data=None, username=None, roomname=None):
 		self.packet = {
 			"opcode": opcode,
 			"data": data,
@@ -31,48 +37,78 @@ class Packet:
 		return json.dumps(self.packet)
 
 
-# Server packets
-class ErrorMessagePacket(Packet):
-	def __init__(self, error_code):
-		super().__init__(opcode='ERROR', data=error_code)
-
-
-class WelcomeMessagePacket(Packet):
-	def __init__(self):
-		super().__init__(opcode='WELCOME', data=None)
-
-
 # Client packets
 class JoinRoomPacket(Packet):
 	def __init__(self, username, roomname):
-		super().__init__(opcode='JOIN_ROOM', data=None, username=username, roomname=roomname)
+		super().__init__(opcode='JOIN_ROOM', username=username, roomname=roomname)
 
 
 class LeaveRoomPacket(Packet):
 	def __init__(self, username, roomname):
-		super().__init__(opcode='LEAVE_ROOM', data=None, username=username, roomname=roomname)
+		super().__init__(opcode='LEAVE_ROOM', username=username, roomname=roomname)
 
 
 class CreateRoomPacket(Packet):
 	def __init__(self, username, roomname):
-		super().__init__(opcode='CREATE_ROOM', data=None, username=username, roomname=roomname)
+		super().__init__(opcode='CREATE_ROOM', username=username, roomname=roomname)
 
 
 class SendMessagePacket(Packet):
-	def __init__(self, username, roomname, data):
-		super().__init__(opcode='SEND_MSG', data=data, username=username, roomname=roomname)
+	def __init__(self, username, roomname, msg):
+		super().__init__(opcode='SEND_MSG', data=msg, username=username, roomname=roomname)
 
 
 class ListRoomsPacket(Packet):
 	def __init__(self, username):
-		super().__init__(opcode='LIST_ROOMS', data=None, username=username, roomname=None)
+		super().__init__(opcode='LIST_ROOMS', username=username)
 
 
 class ListUsersPacket(Packet):
 	def __init__(self, username, roomname):
-		super().__init__(opcode='LIST_USERS', data=None, username=username, roomname=roomname)
+		super().__init__(opcode='LIST_USERS', username=username, roomname=roomname)
 
 
 class ExitPacket(Packet):
 	def __init__(self, username):
-		super().__init__(opcode='EXIT', data=None, username=username, roomname=None)
+		super().__init__(opcode='EXIT', username=username)
+
+
+# Server packets
+class WelcomeMessagePacket(Packet):
+	def __init__(self, msg):
+		super().__init__(opcode='WELCOME', data=msg)
+
+
+class CreateRoomResponsePacket(Packet):
+	def __init__(self, roomname):
+		super().__init__(opcode='CREATE_ROOM_RES', roomname=roomname)
+
+
+class JoinRoomResponsePacket(Packet):
+	def __init__(self, username, roomname):
+		super().__init__(opcode='JOIN_ROOM_RES', username=username, roomname=roomname)
+
+
+class LeaveRoomResponsePacket(Packet):
+	def __init__(self, username, roomname):
+		super().__init__(opcode='LEAVE_ROOM_RES', username=username, roomname=roomname)
+
+
+class ListRoomsResponsePacket(Packet):
+	def __init__(self, room_list):
+		super().__init__(opcode='LIST_ROOMS_RES', data=room_list)
+
+
+class ListUsersResponsePacket(Packet):
+	def __init__(self, roomname, users_list):
+		super().__init__(opcode='LIST_USERS_RES', data=users_list, roomname=roomname)
+
+
+class TellMsgPacket(Packet):
+	def __init__(self, username, roomname, msg):
+		super().__init__(opcode='TELL_MSG', data=msg, username=username, roomname=roomname)
+
+
+class ErrorMessagePacket(Packet):
+	def __init__(self, error_msg):
+		super().__init__(opcode='ERROR', data=error_msg)
